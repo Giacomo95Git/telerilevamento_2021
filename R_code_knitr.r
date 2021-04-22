c8d6e230da0cc198c32f6d01c8645c0dc4fef5a5
Giacomo95Git <80513561+Giacomo95Git@users.noreply.github.com>
2021-04-22 12:03:03 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,7 +1,12 @@
 #R_code_knitr.r
 
 setwd("C:/lab/")
+# la funzione require come la funzione library richiama il pacchetto scelto
+# library o require è indifferente 
 library(knitr)
-
+# la funzione stitch crea automaticamente un report basato su uno script R e uno schema 
+# lo script R è il file "R_code_greenland.r" 
+# il pacchetto knitr è essenziale
+# con stitch viene creato dentro R un report che poi verrà salvato nella cartella .r
 stitch("R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
-
+# copio tutto il codice "time series"
