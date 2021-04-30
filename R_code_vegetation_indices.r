9b11ac8df5e72d2b707ba6e2d62efbdc6abdab08
Giacomo95Git <80513561+Giacomo95Git@users.noreply.github.com>
2021-04-30 09:31:38 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -10,3 +10,16 @@ plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
 #giorno 2
 library(raster)
 setwd("C:/lab/")
+defor1 <- brick("defor1.jpg")
+defor2 <- brick("defor2.jpg")
+par(mfrow=c(2,1))
+plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
+# difference vegetation index 
+dvi1 <- defor1$defor1.1 - defor1$defor1.2
+plot(dvi1)
+dev.off()
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
+plot(dvi1, cl=col)
+
+
