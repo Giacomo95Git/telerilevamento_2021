32bf8b1b20c0bee3c350150bddbae08fd9a3c826
Giacomo95Git <80513561+Giacomo95Git@users.noreply.github.com>
2021-04-30 09:21:03 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -6,3 +6,7 @@ defor2 <- brick("defor2.jpg")
 par(mfrow=c(2,1))
 plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
 plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
+
+#giorno 2
+library(raster)
+setwd("C:/lab/")
