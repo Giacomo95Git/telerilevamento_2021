473e5f0b9682344fa6b100e6831aeb79083a951f
Giacomo95Git <80513561+Giacomo95Git@users.noreply.github.com>
2021-06-30 12:28:50 +0200

    Update R_code_NO2.r

--- R_code_NO2.r
+++ R_code_NO2.r
@@ -4,6 +4,7 @@ setwd("C:/lab/EN")
 # 2. Import the first image (single band)
 #install.packages("raster")
 library(raster)
+library (RStoolbox) 
 EN01 <- raster("EN_0001.png")
 # 3. Plot the image with the preferred Ramp Color Palette
 cls <- colorRampPalette(c("Red", "pink", "orange", "yellow")) (100)
@@ -35,5 +36,35 @@ plot(EN01, col=cls, main="NO2 in January")
 plot(EN13, col=cls, main="NO2 in March")
 plot(ENdif, col=cls, main="Difference (January- March)")
 
+# 7. Import the whole set with a single band
+# tramite list.files creo una lista che rinomino rlist che contiene tutti le immagini dalla 1 alla 13 che hanno in comune la denominazione "EN"
+rlist <- list.files(pattern="EN")
+rlist
 
+# utilizzo la funzione raster tramite la funzione lapply alla mia lista rlist e chiamo il nuovo oggetto "import"
+import <- lapply(rlist, raster)
+import 
+
+EN <- stack(import)
+plot(EN, col=cls)
+
+# 8. Replicate the plot of images 1 and 13 using the stack 
+# parto dallo stack e tramite par plotto le immagini 1 e 13
+# lego l'oggetto EN alla singola banda nominata EN_0001 o EN_0013 tramite $
+# il nome della banda lo vedo semplicemente scrivendo EN e trovandoli nella sezione "names"
+par(mfrow=c(2,1))
+plot(EN$EN_0001, col=cls)
+plot(EN$EN_0013, col=cls)
+
+# 9. Compute a PCA over the 13 images 
+
+ENpca <- rasterPCA(EN)
+
+summary(ENpca$model)
+plotRGB(ENpca$map, r=1, g=2, b=3, stretch="lin")
+
+# 10. Compute the local variabiliy (local standard deviation) of the first PC
+
+PC1sd<- focal(ENpca$map$PC1, w=matrix(1/9, nrow=3, ncol=3), fun=sd)
+plot(PC1sd, col=cls)
 
