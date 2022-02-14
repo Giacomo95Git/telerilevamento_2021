8ef088b39713a298cf75c9eaadd5f1c279cad8ca
Giacomo95Git <80513561+Giacomo95Git@users.noreply.github.com>
2022-02-14 16:37:15 +0100

    Update code.r

--- code.r
+++ code.r
@@ -1,3 +1,8 @@
+# piccolo pezzo di codice sul gran canyon per la classificazione
+# classificazione della stessa immagine
+# classificazione con la funzione unsuperClass
+# sia con 2 classi che con 4 classi per apprezzare le differenze
+# posso plottare anche solo la mappa di un oggetto tramite il comando $
 gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
 plotRGB(gc,r=1,g=2,b= 3,stretch="lin")
 plotRGB(gc,r=1,g=2,b= 3,stretch="hist")
@@ -8,3 +13,7 @@ plot(gcc2$map)
 gcc4 <- unsuperClass(gc, nClasses=4)
 gcc4
 plot(gcc4$map)
+# ricorda che R è key sensitive
+# esempio unsuperclass non esiste perchè la C deve essere maiuscola 
+# unsuperClass funzione giusta
+# occhio sempre anche a parentesi, virgole, spazi ecc.
